f71639866e6190d25eb768da3b266e43f5652af0
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-04 23:57:32 -0400

    Update Header component to have 'New Post' hyperlink

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -4,15 +4,17 @@ import '../App.css';
 import longlogo from '../assets/longlogo.png';
 
 export default function Header() {
+
   return (
     <div id="header-container">
       <div id="header">
-        <Link to="/" id="logo-link"><img id="header-logo" src={longlogo} ></img></Link>
+        <Link to="/" id="logo-link"><img id="header-logo" src={longlogo} alt="CodeHunter logo - large"></img></Link>
         <div id="nav-section">
           <ul>
             <li><Link to="/">HOME</Link></li>
             <li><Link to="/browse">BROWSE</Link></li>
             <li><Link to="/featured">FEATURED</Link></li>
+            <li><Link to={"/new-post"}>NEW POST</Link></li>
           </ul>
         </div>
       </div>
